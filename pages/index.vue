60372992c41e46f027eb790a6412de0c654b7ac5
Hatem Lamine <hatelm902@gmail.com>
2022-07-24 04:44:08 +0100

    feature : now user can create a new workspace

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,38 @@
+<script setup>
+const newWorkspaceName = ref('')
+const workspaceList = ref([{
+ id:1,
+ name : 'pineapple'
+}, {
+  id: 2,
+  name: 'berry'
+ }])
+const createWorkspace = () => {
+ const randomId = Math.floor(Math.random() * 100);
+ const workspaceName = newWorkspaceName;
+ workspaceList.value.push({
+  id: randomId,
+  name: newWorkspaceName.value
+ })
+ }
+</script>
+
+
+
+
+
+
 <template>
  <div>
   <h1>Home Page</h1>
   <h2>Recent Workspaces</h2>
   <h2>Workspaces</h2>
+  <form @submit.prevent="createWorkspace">
+   <input type="text" v-model="newWorkspaceName" placeholder="Type in Workspace Name...">
+   <button type="submit">Create a new workspace</button>
+   <ul>
+    <li v-for="workspace in workspaceList" :key="workspace.id">{{workspace.id}} : {{workspace.name}}</li>
+    </ul>
+  </form>
  </div>
 </template>
